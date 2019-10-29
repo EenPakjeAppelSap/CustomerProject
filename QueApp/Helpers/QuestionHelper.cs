0307fbddf6cc89585b3f6f5b559210f2112f1c1a
NeetjeBroers <nathan.broers2000@gmail.com>
2019-10-29 14:36:57 +0100

    random shit

---
+++ QueApp/Helpers/QuestionHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueApp
+{
+    class QuestionHelper
+    {
+        private static string UserLogin()
+        {
+            var request = WebRequest.Create("https://stemmachine.azurewebsites.net/api/GetParty?code=vNX2uVSyNFCmAhsIoIsinwYaZWwCSVXVprr8nkpTbmyukvhEO5nrdg==");
+            request.ContentType = "application/json";
+            request.Method = "GET";
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return responseString;
+        }
+
+        //private static string SetQuestion()
+        //{
+        //    var request = WebRequest.Create("https://queapp.azurewebsites.net/api/SetQuestion?code=xbd/hedegNoJq0reGeTI1SaTk8S0LvCjbPsev2Nc1xr6tmYl14UtnA==");
+        //    request.ContentType = "application/json";
+        //    request.Method = "POST";
+        //    var response = (HttpWebResponse)request.GetResponse();
+
+        //    var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+        //    return responseString;
+        //}
+
+        private static void SetQuestion(string candidateNumber, string partyNumber)
+        {
+            var request = WebRequest.Create("https://queapp.azurewebsites.net/api/SetQuestion?code=xbd/hedegNoJq0reGeTI1SaTk8S0LvCjbPsev2Nc1xr6tmYl14UtnA==");
+            request.ContentType = "application/json";
+            request.Method = "POST";
+
+            var questionRequest = new 
+            {
+                CandidateNumber = candidateNumber,
+                PartyNumber = partyNumber
+            };
+            var data = JsonConvert.SerializeObject(setQuestionRequest).ToString();
+            using (var writer = new StreamWriter(request.GetRequestStream()))
+            {
+                writer.WriteLine(data);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+        }
+
+    }
+}
